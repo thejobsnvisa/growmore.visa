69c7cf504e3e9dee0715efde40bda7b866fcf848
USER-28 <USER-28@growmore.local>
2026-05-20 12:04:31 +0530

    code updated

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    chunkSizeWarningLimit: 1000, // increase warning limit
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+  },
 });
